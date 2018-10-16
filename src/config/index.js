bb7e63b4f10b32e0a985b6eb9504ec8f66df8efb
Artem <39241034+artemfob@users.noreply.github.com>
2018-10-16 10:28:25 +0300

    Nav2 (#7)
    * Navigation
    
    * Right var
    
    * Last var of basic nav
    
    * Reformat code

---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+const config = {
+  navMenuItems: [
+    {
+      title: 'Films',
+      url: '/films',
+    },
+    {
+      title: 'Serials',
+      url: '/serials',
+    },
+    {
+      title: 'Coming soon',
+      url: 'novelty',
+    },
+  ],
+};
+export default config;
