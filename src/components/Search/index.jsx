a4ea51cf6728d9fb44e4a25ec244741227e3eed7
Feertreesome <ebash.v1.0@gmail.com>
2018-11-18 18:16:46 +0200

    UpdadeSearchView

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import { Navbar, FormGroup, FormControl, Button } from 'react-bootstrap';
+import { Navbar, FormGroup, FormControl, Button, Glyphicon } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 
 import './index.css';
@@ -18,11 +18,13 @@ type Props = {
 
 type State = {
   query: string,
+  input: boolean,
 };
 
 class Search extends Component<Props, State> {
   state = {
     query: '',
+    input: false,
   };
 
   handleChange = (event: SyntheticEvent<HTMLInputElement>) => {
@@ -34,20 +36,36 @@ class Search extends Component<Props, State> {
 
     this.props.history.push(`/search/${this.state.query}`);
   };
+  viweSearch = () => {
+    const { input, query } = this.state;
+    return input ? this.setState({ input: false }) : this.setState({ input: true });
+  };
 
-  render() {
-    const { query } = this.state;
+  renderSearch() {
+    const { query, input } = this.state;
 
-    return (
+    return input ? (
       <form onSubmit={this.handleSubmit}>
         <Navbar.Form pullLeft>
           <FormGroup>
             <FormControl type="search" placeholder="Поиск" defaultValue={query} onChange={this.handleChange} required />
           </FormGroup>{' '}
-          <Button type="submit">Поиск</Button>
+          <Button type="submit" onClick={this.viweSearch}>
+            <Glyphicon glyph="glyphicon glyphicon-search" />
+          </Button>
         </Navbar.Form>
       </form>
+    ) : (
+      <Navbar.Form pullLeft>
+        <Button type="submit" onClick={this.viweSearch}>
+          <Glyphicon glyph="glyphicon glyphicon-search" />
+        </Button>
+      </Navbar.Form>
     );
+  }
+
+  render() {
+    return this.renderSearch();
   }
 }
 
